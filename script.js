118e04b212efe11433aa2d94b2d2b63d3a34bb06
Bodiker <Bodiker86@gmail.com>
2021-04-05 19:01:46 +0200

    a,b,c,d

---
+++ script.js
@@ -0,0 +1,14 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+     count: numberOfFilms,
+     movie:  {},
+     actors: {},
+     genres: [],
+     privat: false
+}; 
+
+ const a = prompt('Один из последних просмотренных фильмов?', ''),
+       b = prompt('На сколько оцените его?', ''),
+       c = prompt('Один из последних просмотренных фильмов?', ''),
+       d = prompt('На сколько оцените его?', '');
