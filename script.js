8f127999499acb9b27478789e9ed83d69176d215
Bodiker <Bodiker86@gmail.com>
2021-04-08 12:21:55 +0200

    while

--- script.js
+++ script.js
@@ -1,34 +1,8 @@
 "Use strict";
 
-// if (1) {
-//     console.log('OK!');
-// } else {
-//     console.log('Error');
-// }
+let num = 50;
 
-// const num = 50;
-
-// if (num < 49) {
-//     console.log('Error');
-// } else if (num > 100) {
-//     console.log('Много');
-// } else {
-//     console.log('OK');
-// }
-
-const num = '50';
-
-switch (num) {
-    case '49':
-        console.log('Неверно');
-        break;
-    case '100':
-        console.log('Неверно');
-        break;
-    case '50':
-        console.log('В точку!');
-        break;
-    default:
-        console.log('Не в этот раз');
-        break;
+while (num <=  55) {
+    console.log(num);
+    num++;
 }
