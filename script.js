42ebbc6e6b0fb0484222ac1ce157b5054b493c46
Bodiker <Bodiker86@gmail.com>
2021-04-10 16:15:19 +0200

    Full metodic

--- script.js
+++ script.js
@@ -37,14 +37,20 @@ function rememberMyFilms() {
 
 rememberMyFilms();
 
-if (personalMovieDB.count < 10) {
-    console.log("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Вы классичесий зритель");
-} else if (personalMovieDB.count >= 30) {
-    console.log("Вы киноман");
-} else {
-    console.log("Произошла ошибка");
+function detectPersonalLevel (){
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");  
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Вы классичесий зритель");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Вы киноман");
+    } else {
+        console.log("Произошла ошибка");
+    }
+
+
 }
+
+detectPersonalLevel();
 
 console.log(personalMovieDB);
