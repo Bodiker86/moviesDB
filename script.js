2df654a6f3e2281773d9ab7b7c8f2594899ddb73
Bodiker <Bodiker86@gmail.com>
2021-04-10 17:06:11 +0200

    genres video 18

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ const personalMovieDB = {
 
 function rememberMyFilms() {
     for(let i = 0; i < 2; i++) {
-        const a = prompt('Один из полследних просмотренных фильмов?', ''),
-              b = prompt('На сколько оцкниваите его?', '');
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('На сколько оцениваите его?', '');
         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
             personalMovieDB.movies[a] = b;
             console.log('done');
@@ -53,4 +53,20 @@ function detectPersonalLevel (){
 
 detectPersonalLevel();
 
-console.log(personalMovieDB);
+function showMyDB (hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+
+
+showMyDB(personalMovieDB.privat);
+
+ function writeYourGenres() {
+     for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+     }
+ }
+
+ writeYourGenres();
