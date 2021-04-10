3df40769484a4ccb798deeaa26a0a472c2405f70
Bodiker <Bodiker86@gmail.com>
2021-04-10 14:35:09 +0200

    metodic

--- script.js
+++ script.js
@@ -1,38 +1,7 @@
 "Use strict";
 
-let num = 20;
+const str = "test";
+const arr = [1, 2, 4];
 
-function showFirstMassage(text) {
-    console.log(text);
-    num = 10;
-}
-
-showFirstMassage("Hello Bodya!");
-console.log (num);
-
-function calc(a, b) {
-    return (a + b);
-}
-
-console.log(calc(4, 3));
-console.log(calc(5, 6));
-console.log(calc(10, 6));
-console.log(calc(22, 11));
-
-function ret () {
-    let num = 50;
-    
-    //
-
-    return num;
-}
-
-
-const anotherNum = ret();
-console.log(anotherNum);
-
-const logger = function(){
-    console.log("Hello");
-};
-
-logger();
+console.log(str[2] = 'd');
+console.log(str);
