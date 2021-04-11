e2296aedce5e7939bdaae2c85dc747b8950aa5d9
Bodiker <Bodiker86@gmail.com>
2021-04-11 19:39:03 +0200

    Options key

--- script.js
+++ script.js
@@ -10,12 +10,18 @@ const options = {
     }
 };
 
-console.log (options.name);
+// console.log (options.name);
+// console.log(options);
 
-delete options.name;
-
-console.log(options);
-
-delete options.width;
-
-console.log(options);
+for (let key in options) {
+    if (typeof(options[key]) === 'object') {
+        for (let i in options [key]) {
+            console.log(`Свойства ${key} имеит значения ${options [key] [i]}`);
+         }
+    }  else  {
+        console.log(`Свойства ${key} имеит значения ${options [key]}`);
+    }
+    
+}
+  
+  
