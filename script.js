37ad66e716e84f650e9f7c3f851675f823dee28e
Bodiker <Bodiker86@gmail.com>
2021-04-11 19:59:19 +0200

    Options dicstr

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
-"Use strict";
+ "Use strict";
 
-const options = {
+const  options = {
     name: 'test',
     width: 1024,
     heigth: 1024,
     colors: {
         border: 'black',
         bg: 'red'
+    },
+    makeTest: function(){
+        console.log('Test');
     }
 };
 
+options.makeTest();
+
+const {border, bg} = options.colors;
+
+// console.log(Object.keys(options));
+
 // console.log (options.name);
+
 // console.log(options);
 
-for (let key in options) {
-    if (typeof(options[key]) === 'object') {
-        for (let i in options [key]) {
-            console.log(`Свойства ${key} имеит значения ${options [key] [i]}`);
-         }
-    }  else  {
-        console.log(`Свойства ${key} имеит значения ${options [key]}`);
-    }
+// let counter = 0;
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let i in options [key]) {
+//             console.log(`Свойства ${key} имеит значения ${options [key] [i]}`);
+//          }
+//     }  else  {
+//         console.log(`Свойства ${key} имеит значения ${options [key]}`);
+//         counter++;
+//     }
     
-}
-  
+// }
+//   console.log (counter);
   
