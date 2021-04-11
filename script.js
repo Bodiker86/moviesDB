c6c6e205e1d8a2593bcab5b5a7cad7e79fe3f00b
Bodiker <Bodiker86@gmail.com>
2021-04-11 15:13:40 +0200

    Callback lesson 19

--- script.js
+++ script.js
@@ -1,72 +1,24 @@
 "Use strict";
 
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-    }
+function first () {
+    //Do something
+    setTimeout(function() {
+        console.log(1);
+    },500);
 }
 
-start(); 
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    print: false
-};
-
-function rememberMyFilms() {
-    for(let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
-              b = prompt('На сколько оцениваите его?', '');
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else {
-            console.log('error');
-            i--;
-        }
-    }
+function second(){
+    console.log(2);
 }
 
-rememberMyFilms();
+first();
+second();
 
-function detectPersonalLevel (){
-    if (personalMovieDB.count < 10) {
-        console.log("Просмотрено довольно мало фильмов");  
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log("Вы классичесий зритель");
-    } else if (personalMovieDB.count >= 30) {
-        console.log("Вы киноман");
-    } else {
-        console.log("Произошла ошибка");
-    }
-
-
+function learnJS(lang, callback) {
+    console.log(`Я учу: ${lang}`);
+    callback();
 }
 
-detectPersonalLevel();
-
-function showMyDB (hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
-
-
-
-showMyDB(personalMovieDB.privat);
-
- function writeYourGenres() {
-     for (let i = 1; i <= 3; i++) {
-        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
-     }
- }
-
- writeYourGenres();
+learnJS('javaScript', function(){
+    console.log('Я прошел этот урок!');
+});
