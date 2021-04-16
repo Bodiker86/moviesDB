c227f0b390194f37006a596d46ebae08c8af3bdd
Bodiker <Bodiker86@gmail.com>
2021-04-16 21:24:16 +0200

    Task

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
 "Use strict";
 
-// 1)
-console.log(typeof(String(null)));
-console.log(typeof(String(4)));
+// let x = 5; alert( x++);
 
-// 2)
-console.log(typeof(5 + ''));
 
-const num = 5;
+// [ ] + false - null + true
 
-console.log ("https://vk.com/catalog/"+ num);
+// let y = 1;
+// let x = y = 2; 
+// alert(x); 
 
-const fontSize = 26 + 'px';
+// console.log([ ] + 1 + 2); = 12
 
-// To Number
-console.log(typeof(Number('4')));
+// alert( "1"[0] ); = 1
 
-// 2)
-console.log(typeof(+'5'));
+//console.log(2 && 1 && null && 0 && undefined ); = null
 
-// 3)
-console.log(typeof(parseInt("15px", 10)));
+// let a ;
+// let b ;
+// console.log( a && b ); = false, undefined
+
+//alert( null || 2 && 3 || 4 ); = 3
+
+let a;
+let b;
+ a = [1, 2, 3]; b = [1, 2, 3]; 
+ console.log (a==b); 
+
+alert( +"Infinity" );  
+
+ console.log("Ёжик" > "яблоко"); 
+
+ console.log(0 || "" || 2 || undefined || true || false); 
